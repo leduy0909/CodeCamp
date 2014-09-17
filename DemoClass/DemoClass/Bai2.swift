82dbca261a43097838c6093904fe7739fdddce51
Mettamdaica0909 <mettamdaica0909@gmail.com>
2014-09-17 11:19:33 +0700

    CodeCamp

--- DemoClass/DemoClass/Bai2.swift
+++ DemoClass/DemoClass/Bai2.swift
@@ -12,14 +12,7 @@ class Bai2: ConsoleScreen {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.khongtrung()
+
         // Do any additional setup after loading the view.
-    }
-    func khongtrung() {
-        var intArr = [1, 2, 5, 2, 11, 25, 4, 11, 1, 5, 11, 3]
-        intArr.khongtrung()
-        for i in intArr {
-            self.write("\(i)  ")
-        }
     }
 }
